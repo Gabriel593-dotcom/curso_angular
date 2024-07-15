1f367ed8e434bbc151f191e777ae3e642703433e
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2024-07-15 19:47:04 -0300

    local reference

--- projects/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ projects/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -3,12 +3,14 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css', 
-  encapsulation: ViewEncapsulation.Emulated
+              styleUrl: './server-element.component.css',
+        encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent {
-  @Input('srvElement')
-  element: {type: string, name: string, content: string};
 
-  constructor() {}
+
+  @Input('srvElement')
+  element: { type: string, name: string, content: string };
+
+  constructor() { }
 }
