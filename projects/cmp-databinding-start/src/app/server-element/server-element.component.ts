afdf3d527f88f921927edf1ce46c97bf3e7bdd69
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2024-07-26 19:23:09 -0300

    Projecting Content into Components with ng-content

--- projects/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ projects/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
               styleUrl: './server-element.component.css',
-        encapsulation: ViewEncapsulation.Emulated
+        encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ServerElementComponent {
 
